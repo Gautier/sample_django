ca11640fd224d29da8ab694fb514740898cb029e
Gautier Hayoun <ghayoun@gmail.com>
2011-04-16 18:02:50 +0100

    Added libs as a directory to store external libs (ie django)

--- django_wsgi.py
+++ django_wsgi.py
@@ -1,12 +1,16 @@
+# first configure the path to add our bundled django, and other libraries
+import sys
 import os
-import django.core.handlers.wsgi
-from wsgiref.simple_server import make_server
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'libs'))
 
 # get the django wsgi app
+import django.core.handlers.wsgi
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 application = django.core.handlers.wsgi.WSGIHandler()
 
-# serve it with wsgiref, let cloudfoundry do the rest :)
+# then serve it with wsgiref, let cloudfoundry do the rest :)
+from wsgiref.simple_server import make_server
 PORT = int(os.environ["VCAP_APP_PORT"])
 httpd = make_server('', PORT, application)
 print "Serving HTTP on port %d..." % PORT
